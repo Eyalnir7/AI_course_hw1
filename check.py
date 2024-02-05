589d25192659a802cd5d7c6e5e6c8109de971b92
Eyal <tadmoreyal@gmail.com>
2024-02-05 12:47:16 +0200

    Initial commit

---
+++ check.py
@@ -0,0 +1,142 @@
+import ex1
+import search
+import time
+
+
+def timeout_exec(func, args=(), kwargs={}, timeout_duration=10, default=None):
+    """This function will spawn a thread and run the given function
+    using the args, kwargs and return the given default value if the
+    timeout_duration is exceeded.
+    """
+    import threading
+
+    class InterruptableThread(threading.Thread):
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.result = default
+
+        def run(self):
+            # try:
+            self.result = func(*args, **kwargs)
+            # except Exception as e:
+            #    self.result = (-3, -3, e)
+
+    it = InterruptableThread()
+    it.start()
+    it.join(timeout_duration)
+    if it.is_alive():
+        return default
+    else:
+        return it.result
+
+
+def check_problem(p, search_method, timeout):
+    """ Constructs a problem using ex1.create_wumpus_problem,
+    and solves it using the given search_method with the given timeout.
+    Returns a tuple of (solution length, solution time, solution)"""
+
+    """ (-2, -2, None) means there was a timeout
+    (-3, -3, ERR) means there was some error ERR during search """
+
+    t1 = time.time()
+    s = timeout_exec(search_method, args=[p], timeout_duration=timeout)
+    t2 = time.time()
+
+    if isinstance(s, search.Node):
+        solve = s
+        solution = list(map(lambda n: n.action, solve.path()))[1:]
+        return (len(solution), t2 - t1, solution)
+    elif s is None:
+        return (-2, -2, None)
+    else:
+        return s
+
+
+def solve_problems(problems):
+    solved = 0
+    for problem in problems:
+        try:
+            p = ex1.create_onepiece_problem(problem)
+        except Exception as e:
+            print("Error creating problem: ", e)
+            return None
+        timeout = 60
+        result = check_problem(
+            p, (lambda p: search.astar_search(p, p.h)), timeout)
+        print("A* ", result)
+        if result[2] != None:
+            if result[0] != -3:
+                solved = solved + 1
+
+
+def main():
+    print(ex1.ids)
+    """Here goes the input you want to check"""
+    problems = [
+        {
+            "map": [
+                ['S', 'S', 'I', 'S'],
+                ['S', 'S', 'S', 'S'],
+                ['B', 'S', 'S', 'S'],
+                ['S', 'S', 'S', 'S']
+            ],
+            "pirate_ships": {"pirate_ship_1": (2, 0)},
+            "treasures": {'treasure_1': (0, 2)},
+            "marine_ships": {'marine_1': [(1, 1), (1, 2), (2, 2), (2, 1)]}
+        },
+
+        {
+            "map": [
+                ['S', 'S', 'I', 'S', 'S'],
+                ['S', 'S', 'S', 'S', 'S'],
+                ['B', 'S', 'S', 'S', 'S'],
+                ['S', 'S', 'S', 'I', 'S'],
+                ['S', 'S', 'S', 'S', 'S']
+            ],
+            "pirate_ships": {"pirate_ship_1": (2, 0)},
+            "treasures": {'treasure_1': (0, 2), 'treasure_2': (3, 3)},
+            "marine_ships": {
+                'marine_1': [(1, 1), (1, 2), (2, 2), (2, 1), (3, 1)],
+                'marine_2': [(3, 4), (2, 4), (1, 4), (1, 3), (2, 3)]
+            }
+        },
+        {
+            'map': [['S', 'S', 'S', 'S', 'I'],
+                    ['S', 'I', 'S', 'S', 'S'],
+                    ['S', 'S', 'S', 'S', 'S'],
+                    ['B', 'S', 'S', 'I', 'S']],
+            'pirate_ships': {'pirate_ship_1': (3, 0)},
+            'treasures': {'treasure_1': (3, 3), 'treasure_2': (1, 1), 'treasure_3': (0, 4)},
+            'marine_ships': {'marine_1': [(3, 2)], 'marine_2': [(0, 2), (0, 3)], 'marine_3': [(3, 4), (2, 4)]},
+        },
+        {
+            'map': [['S', 'S', 'S', 'S', 'I'],
+                    ['S', 'I', 'S', 'S', 'S'],
+                    ['S', 'S', 'S', 'S', 'S'],
+                    ['B', 'S', 'S', 'I', 'S']],
+            'pirate_ships': {'pirate_ship_1': (3, 0)},
+            'treasures': {'treasure_1': (1, 1), 'treasure_2': (3, 3)},
+            'marine_ships': {'marine_1': [(2, 3), (2, 4)], 'marine_2': [(0, 1), (0, 0)], 'marine_3': [(0, 0), (0, 1)],
+                             'marine_4': [(2, 4), (2, 3)]},
+        },
+        {
+            'map': [['S', 'S', 'S', 'I', 'S', 'S', 'S'],
+                    ['S', 'I', 'S', 'S', 'S', 'S', 'I'],
+                    ['S', 'S', 'I', 'S', 'S', 'I', 'S'],
+                    ['S', 'B', 'S', 'I', 'S', 'S', 'S'],
+                    ['S', 'S', 'S', 'S', 'S', 'I', 'S'],
+                    ['S', 'S', 'S', 'I', 'S', 'S', 'S']],
+            'pirate_ships': {'pirate_ship_1': (3, 1)},
+            'treasures': {'treasure_1': (4, 5), 'treasure_2': (1, 1), 'treasure_3': (1, 6)},
+            'marine_ships': {'marine_1': [(4, 1), (4, 2)], 'marine_2': [(3, 5), (3, 4), (4, 4)],
+                             'marine_3': [(1, 2), (1, 3), (1, 4)], 'marine_4': [(5, 6)]},
+        },
+
+
+    ]
+
+    solve_problems(problems)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ex1.py
@@ -0,0 +1,136 @@
+import search
+import random
+import math
+
+
+ids = ["111111111", "111111111"]
+
+
+class OnePieceProblem(search.Problem):
+    """This class implements a medical problem according to problem description file"""
+
+    def __init__(self, initial):
+        """Don't forget to implement the goal test
+        You should change the initial to your own representation.
+        search.Problem.__init__(self, initial) creates the root node"""
+        initial_state = OnePieceState(initial["pirate_ships"], initial["treasures"],
+                                      [ship[0] for ship in initial["marine_ships"].values()])
+        search.Problem.__init__(self, initial_state)
+        self.map = initial["map"]
+        self.pirate_ships = initial["pirate_ships"]
+        self.treasures = initial["treasures"]
+        self.marine_ships = initial["marine_ships"]
+
+    def atomic_actions(self, ship, state):
+        """
+        Returns all the atomic actions that can be executed by a ship
+        :param ship: should be a tuple (ship_name, (x, y))
+        :param state: should be a OnePieceState object
+        """
+        actions = [("sail", ship[0], (ship[1][0] + 1, ship[1][1])), ("sail", ship[0], (ship[1][0] - 1, ship[1][1])),
+                   ("sail", ship[0], (ship[1][0], ship[1][1] + 1)), ("sail", ship[0], (ship[1][0], ship[1][1] - 1))]
+        # sails actions
+        for action in actions:
+            if not self.check_sail(action):
+                actions.remove(action)
+
+        # wait action
+        actions.append(("wait", ship[0]))
+
+        # collect treasure action
+        if self.check_collect_treasure(ship, state):
+            actions.append(("collect_treasure", ship[0]))
+
+        # deposit treasure action
+        if self.check_collect_treasure(ship, state):
+            actions.append(("deposit_treasure", ship[0]))
+
+        return actions
+
+    def check_collect_treasure(self, ship, state):
+        """
+        Checks if the ship can collect a treasure
+        :param ship: should be a tuple (ship_name, (x, y))
+        :param state: the current state (OnePieceState object)
+        """
+        if len([treasure for treasure in state.treasures if state.treasures[treasure] == ship[1]]) == 2:
+            return False
+        if ship[1][0]+1 < len(self.map) and self.map[ship[1][0]+1][ship[1][1]] == "I":
+            return True
+        if ship[1][0]-1 >= 0 and self.map[ship[1][0]-1][ship[1][1]] == "I":
+            return True
+        if ship[1][1]+1 < len(self.map[0]) and self.map[ship[1][0]][ship[1][1]+1] == "I":
+            return True
+        if ship[1][1]-1 >= 0 and self.map[ship[1][0]][ship[1][1]-1] == "I":
+            return True
+        return False
+
+    def check_sail(self, action):
+        """
+        Checks if the sail action is valid
+        :param action: tuple of ("sail", ship_name, (x, y))
+        """
+        if action[2][0] < 0 or action[2][1] < 0 or action[2][0] >= len(self.map) or action[2][1] >= len(self.map[0]):
+            return False
+        if self.map[action[2][0]][action[2][1]] == "I":
+            return False
+        return True
+
+    def check_deposit_treasure(self, ship, state):
+        """
+        Checks if the ship can deposit a treasure
+        :param ship: should be a tuple (ship_name, (x, y))
+        :param state: the current state (OnePieceState object)
+        """
+        return self.map[ship[1][0]][ship[1][1]] == "B"
+
+    def actions(self, state):
+        """Returns all the actions that can be executed in the given
+        state. The result should be a tuple (or other iterable) of actions
+        as defined in the problem description file"""
+        actions = []
+        for ship in state.pirate_ships:
+            actions.append(self.atomic_actions((ship, state.pirate_ships[ship]), state))
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+
+    def goal_test(self, state):
+        """ Given a state, checks if this is the goal state.
+         Returns True if it is, False otherwise."""
+
+    def h(self, node):
+        """ This is the heuristic. It gets a node (not a state,
+        state can be accessed via node.state)
+        and returns a goal distance estimate"""
+        return 0
+
+    """Feel free to add your own functions
+    (-2, -2, None) means there was a timeout"""
+
+
+class OnePieceState:
+    def __init__(self, pirate_ships, treasures, marine_ships):
+        self.pirate_ships = pirate_ships
+        self.treasures = treasures
+        self.marine_ships = marine_ships
+
+    def __eq__(self, other):
+        return self.pirate_ships == other.pirate_ships and self.treasures == other.treasures and self.marine_ships == other.marine_ships
+
+    def __hash__(self):
+        return hash((self.pirate_ships, self.treasures, self.marine_ships))
+
+    def __str__(self):
+        return f"Pirate ships: {self.pirate_ships}, Treasures: {self.treasures}, Marine ships: {self.marine_ships}"
+
+    def __repr__(self):
+        return f"State: Pirate ships: {self.pirate_ships}, Treasures: {self.treasures}, Marine ships: {self.marine_ships}"
+
+
+
+def create_onepiece_problem(game):
+    return OnePieceProblem(game)
+
