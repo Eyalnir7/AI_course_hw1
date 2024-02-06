962214ebfd0b068cc86617b1b938c13498fc964f
Eyal <tadmoreyal@gmail.com>
2024-02-06 23:02:14 +0200

    updated state class and result

--- ex1.py
+++ ex1.py
@@ -2,9 +2,34 @@ import search
 import random
 import math
 from itertools import product
+import copy
 
 
 ids = ["111111111", "111111111"]
+
+class OnePieceState:
+    def __init__(self, pirate_ships, marine_ships, treasures_in_base):
+        self.pirate_ships = pirate_ships
+        self.marine_ships = marine_ships
+        self.treasures_in_base = treasures_in_base
+
+    def __eq__(self, other):
+        return self.pirate_ships == other.pirate_ships  and self.marine_ships == other.marine_ships
+
+    def __hash__(self):
+        return hash((self.pirate_ships, self.marine_ships, self.treasures_in_base))
+
+    def __str__(self):
+        return f"Pirate ships: {self.pirate_ships}, Marine ships: {self.marine_ships}, " \
+               f"treasures in base: {self.treasures_in_base}"
+
+    def __repr__(self):
+        return f"State: Pirate ships: {self.pirate_ships}, Marine ships: {self.marine_ships}, " \
+               f"treasures in base: {self.treasures_in_base}"
+
+    def __deepcopy__(self, memodict={}):
+        return OnePieceState(copy.deepcopy(self.pirate_ships),copy.deepcopy(self.marine_ships),
+                             copy.deepcopy(self.treasures_in_base))
 
 
 class OnePieceProblem(search.Problem):
@@ -14,18 +39,21 @@ class OnePieceProblem(search.Problem):
         """Don't forget to implement the goal test
         You should change the initial to your own representation.
         search.Problem.__init__(self, initial) creates the root node"""
-        initial_state = OnePieceState(initial["pirate_ships"], initial["treasures"],
-                                      [ship[0] for ship in initial["marine_ships"].values()])
+        initial["pirate_ships"] = {ship: [values, set()]
+                                   for ship, values in initial["pirate_ships"].items()}
+        initial_state = OnePieceState(initial["pirate_ships"],
+                                      {key: value[0] for key, value in initial["marine_ships"].items()}, set())
         search.Problem.__init__(self, initial_state)
         self.map = initial["map"]
         self.pirate_ships = initial["pirate_ships"]
         self.treasures = initial["treasures"]
-        self.marine_ships = initial["marine_ships"]
+        self.initial_marine_ships = initial["marine_ships"]
+        self.marine_ships = self.initial_marine_ships
 
-    def atomic_actions(self, ship, state):
+    def atomic_actions(self, ship):
         """
         Returns all the atomic actions that can be executed by a ship
-        :param ship: should be a tuple (ship_name, (x, y))
+        :param ship: should be a tuple (ship_name, (x, y), set of treasures)
         :param state: should be a OnePieceState object
         """
         actions = [("sail", ship[0], (ship[1][0] + 1, ship[1][1])), ("sail", ship[0], (ship[1][0] - 1, ship[1][1])),
@@ -39,22 +67,22 @@ class OnePieceProblem(search.Problem):
         actions.append(("wait", ship[0]))
 
         # collect treasure action
-        if self.check_collect_treasure(ship, state):
+        if self.check_collect_treasure(ship):
             actions.append(("collect_treasure", ship[0]))
 
         # deposit treasure action
-        if self.check_collect_treasure(ship, state):
+        if self.check_deposit_treasure(ship):
             actions.append(("deposit_treasure", ship[0]))
 
         return actions
 
-    def check_collect_treasure(self, ship, state):
+    def check_collect_treasure(self, ship):
         """
         Checks if the ship can collect a treasure
-        :param ship: should be a tuple (ship_name, (x, y))
+        :param ship: should be a tuple (ship_name, (x, y), set of treasures)
         :param state: the current state (OnePieceState object)
         """
-        if len([treasure for treasure in state.treasures if state.treasures[treasure] == ship[1]]) == 2:
+        if len(ship[2]) == 2:
             return False
         if ship[1][0]+1 < len(self.map) and self.map[ship[1][0]+1][ship[1][1]] == "I":
             return True
@@ -77,7 +105,7 @@ class OnePieceProblem(search.Problem):
             return False
         return True
 
-    def check_deposit_treasure(self, ship, state):
+    def check_deposit_treasure(self, ship):
         """
         Checks if the ship can deposit a treasure
         :param ship: should be a tuple (ship_name, (x, y))
@@ -91,13 +119,36 @@ class OnePieceProblem(search.Problem):
         as defined in the problem description file"""
         actions = []
         for ship in state.pirate_ships:
-            actions.append(self.atomic_actions((ship, state.pirate_ships[ship]), state))
+            actions.append(self.atomic_actions((ship, *state.pirate_ships[ship])))
         return list(product(*actions))
 
-    def result(self, state, action):
+    def result(self, state: OnePieceState, action):
         """Return the state that results from executing the given
         action in the given state. The action must be one of
         self.actions(state)."""
+        new_state = copy.deepcopy(state)
+        self.move_marine_ships(new_state)
+        for a in action:
+            if a[0] == "sail":
+                new_state.pirate_ships[a[1]][0] = a[2]
+            if a[0] == "collect_treasure":
+                new_state.pirate_ships[a[1]][1].add(a[2])
+            if a[0] == "deposit_treasure":
+                new_state.treasures_in_base = new_state.treasures_in_base.union(new_state.pirate_ships[a[1]][1])
+                new_state.pirate_ships[a[1]][1] = set()
+
+        marine_locations = new_state.marine_ships.values()
+        for ship, value in new_state.pirate_ships.items():
+            if value[0] in marine_locations:
+                new_state.pirate_ships[ship][1] = set()
+
+    def move_marine_ships(self, state):
+        for ship in self.marine_ships:
+            cur_index = self.marine_ships[ship].index(state.marine_ships[ship])
+            if cur_index == len(self.marine_ships[ship])-1:
+                cur_index = 0
+                self.marine_ships[ship] = self.marine_ships[ship][::-1]
+            state.marine_ships[ship] = self.marine_ships[ship][self.marine_ships[cur_index]+1]
 
     def goal_test(self, state):
         """ Given a state, checks if this is the goal state.
@@ -111,25 +162,6 @@ class OnePieceProblem(search.Problem):
 
     """Feel free to add your own functions
     (-2, -2, None) means there was a timeout"""
-
-
-class OnePieceState:
-    def __init__(self, pirate_ships, treasures, marine_ships):
-        self.pirate_ships = pirate_ships
-        self.treasures = treasures
-        self.marine_ships = marine_ships
-
-    def __eq__(self, other):
-        return self.pirate_ships == other.pirate_ships and self.treasures == other.treasures and self.marine_ships == other.marine_ships
-
-    def __hash__(self):
-        return hash((self.pirate_ships, self.treasures, self.marine_ships))
-
-    def __str__(self):
-        return f"Pirate ships: {self.pirate_ships}, Treasures: {self.treasures}, Marine ships: {self.marine_ships}"
-
-    def __repr__(self):
-        return f"State: Pirate ships: {self.pirate_ships}, Treasures: {self.treasures}, Marine ships: {self.marine_ships}"
 
 
 
